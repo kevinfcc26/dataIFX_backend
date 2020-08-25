0ae12765e807abf383983daf547337c5cf53a2a3
kevinfcc26 <kevinfcc26@gmail.com>
2020-08-24 21:07:57 -0500

    first commit

---
+++ WindowsFolders 123/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WindowsFolders_123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ prueba2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace prueba2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
